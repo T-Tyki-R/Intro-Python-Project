057c942d59731a39ef59e368a91439e3bcd16bd5
Tameka Roberson <tamekaroberson1995@gmail.com>
2025-04-21 21:44:57 -0400

    Assignment Completed

--- To-Do-List.py
+++ To-Do-List.py
@@ -8,7 +8,7 @@ task_stat = []
 def add_task():
     task = input("enter a task you want to add: ").capitalize()
     task_list.append(task)
-    task_stat.append("incomplete") 
+    task_stat.append("Incompleted") 
     print(f"'{task}' added to the list.")
 
 # Remove Task from List
@@ -24,7 +24,7 @@ def check_status():
     task = input("Which task do you wish to check on?: ").capitalize()
     if task in task_list:
         task_found = task_list.index(task)
-        task_stat[task_found] = "complete"
+        task_stat[task_found] = "Completed"
     else:
         print(f"'{task}' isn't in the list.")
 
@@ -37,3 +37,27 @@ def display_tasks():
             print(f"{i + 1}. {task_list[i]} - {task_stat[i]}")
 
 # Application Menu
+
+while True:
+    print("\tMenu\n1. Add a Task\n2. Remove a Tasks\n3. Mark a Task as Complete\n4. Diplay List\n5. Quit\n What would you like to do?")
+    try:
+        user_choice = int(input("Select a number from the menu: "))
+    except ValueError:
+        print("Please enter a number between 1 - 5.")
+    else:
+        match user_choice: 
+            case 1:      
+                add_task()
+                pass
+            case 2:
+                remove_task()
+                pass
+            case 3:
+                check_status()  
+                pass                 
+            case 4:
+                display_tasks()
+                pass
+            case 5:
+                print("Thank you for using Task Management Pro. Have a great rest of your day!")
+                break
