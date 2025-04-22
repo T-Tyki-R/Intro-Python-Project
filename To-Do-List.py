ccb9ef26c0d4fd623644ad44c4ec9bac33b3e1d9
Tameka Roberson <tamekaroberson1995@gmail.com>
2025-04-21 21:36:19 -0400

    Check Status Function Completed

--- To-Do-List.py
+++ To-Do-List.py
@@ -6,20 +6,27 @@ task_stat = []
 
 # Add Task to List
 def add_task():
-    task = input("enter a task you want to add: ")
+    task = input("enter a task you want to add: ").capitalize()
     task_list.append(task)
     task_stat.append("incomplete") 
     print(f"'{task}' added to the list.")
 
 # Remove Task from List
 def remove_task():
-    task = input("What task do you wish to remove?: ")
+    task = input("What task do you wish to remove?: ").capitalize()
     if task in task_list:
         task_list.remove(task)
     else:
         print(f"'{task}' isn't in the list.")
 
 # Check Task Status
+def check_status():
+    task = input("Which task do you wish to check on?: ").capitalize()
+    if task in task_list:
+        task_found = task_list.index(task)
+        task_stat[task_found] = "complete"
+    else:
+        print(f"'{task}' isn't in the list.")
 
 # Display Task List
 
