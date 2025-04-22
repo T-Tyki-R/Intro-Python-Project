dbc65a8cd71046e47ad8e98f2e52b4663c6486dc
Tameka Roberson <tamekaroberson1995@gmail.com>
2025-04-21 21:32:41 -0400

    Remove Task Function Completed and Add Task Function Updated

--- To-Do-List.py
+++ To-Do-List.py
@@ -6,12 +6,18 @@ task_stat = []
 
 # Add Task to List
 def add_task():
-    task = input("enter a task you want to add:")
+    task = input("enter a task you want to add: ")
     task_list.append(task)
     task_stat.append("incomplete") 
     print(f"'{task}' added to the list.")
 
 # Remove Task from List
+def remove_task():
+    task = input("What task do you wish to remove?: ")
+    if task in task_list:
+        task_list.remove(task)
+    else:
+        print(f"'{task}' isn't in the list.")
 
 # Check Task Status
 
