233ed12704b0d666ac9c4057c010c6d51147df76
Tameka Roberson <tamekaroberson1995@gmail.com>
2025-04-21 21:28:58 -0400

    Add Task Function Completed

--- To-Do-List.py
+++ To-Do-List.py
@@ -1,2 +1,20 @@
 # Make 2 empty lists, one for the tasks and one for the status of the tasks
 # Use Loops (While), Switch case, and If statements to create the menu and the options
+
+task_list = []
+task_stat = []
+
+# Add Task to List
+def add_task():
+    task = input("enter a task you want to add:")
+    task_list.append(task)
+    task_stat.append("incomplete") 
+    print(f"'{task}' added to the list.")
+
+# Remove Task from List
+
+# Check Task Status
+
+# Display Task List
+
+# Appplication Menu
